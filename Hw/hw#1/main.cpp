732ce3a9cdfacaadc2ee9508212e6f6d3862645d
dbsduwns <41357828+dbsduwns@users.noreply.github.com>
2024-09-30 14:26:29 +0900

    Update main.cpp

--- Hw/hw#1/main.cpp
+++ Hw/hw#1/main.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
 
+#include "Calculator.h"
+
+int main() {
+	Add a; Sub s; Mul m; Div d;
+	int x, y;
+	char Cal;
+
+	while (true) {
+		cout << "계산기 프로그램" << endl;
+		cout << "두 정수와 연산자를 입력해주세요 : ";
+		cin >> x >> y >> Cal;
+
+		switch (Cal) {
+		case '+':
+			a.set(x, y);
+			cout << "계산결과 : " << a.calculate() << endl;
+			break;
+		case '-':
+			s.set(x, y);
+			cout << "계산결과 : " << s.calculate() << endl;
+			break;
+		case '*':
+			m.set(x, y);
+			cout << "계산결과 : " << m.calculate() << endl;
+			break;
+		case '/':
+			d.set(x, y);
+			cout << "계산결과 : " << d.calculate() << endl;
+			break;
+		}
+	}
+}
