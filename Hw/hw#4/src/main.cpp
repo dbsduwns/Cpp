2ed4908ab1c526d0d6c68ff671fe6075fd6373c5
dbsduwns <41357828+dbsduwns@users.noreply.github.com>
2024-11-10 18:32:08 +0900

    Update main.cpp

--- Hw/hw#4/src/main.cpp
+++ Hw/hw#4/src/main.cpp
@@ -2,106 +2,138 @@
 #include <string>
 using namespace std;
 
-class printer {
-protected:
-    string model;
-    string manufacturer;
-    int printedCount = 0;
-    int availableCount;
+class Printer {
+	string model;
+	string manufactuer;
+	int printedCount, availableCount;//printedCount:인쇄할 종이양, availableCount: 남은 종이
 public:
-    printer(int pages) : availableCount(pages) {}
-    void setModel(string model) { this->model = model; }
-    void setManufacturer(string manufacturer) { this->manufacturer = manufacturer; }
-    string getModel() { return model; }
-    string getManufacturer() { return manufacturer; }
-    int getPages() { return availableCount; }
-    void print(int pages) {
-        if (pages > availableCount) {
-            cout << "종이가 부족합니다." << endl;
-            return;
-        }
-        printedCount += pages;
-        availableCount -= pages;
-        cout << "프린트하였습니다." << endl;
-    }
+	Printer() {
+		this->model = "";
+		this->manufactuer = "";
+		this->availableCount = 0;
+		this->printedCount = 0;
+	}
+	Printer(string model, string manuf, int count) {
+		this->model = model;
+		this->manufactuer = manuf;
+		this->availableCount = count;
+	}
+	bool print(int pages) {
+		if (pages > availableCount) {
+			cout << "용지가 부족하여 프린트를 할 수 없습니다." << endl;
+			return false;
+		}
+		else {
+			this->printedCount += pages;
+			this->availableCount -= pages;
+			return true;
+		}
+	}
+	string getManu() { return manufactuer; }
+	string getModel() { return model; }
+	int getCount() { return availableCount; }
 };
 
-class printerInkJet : public printer {
-    int availableInk;
+class Ink :virtual public Printer {
+	int availableInk;//남은 잉크양
 public:
-    printerInkJet(int pages, int availableInk) : printer(pages), availableInk(availableInk) {}
-    int getInk() { return availableInk; }
-    void printInkJet(int pages) {
-        print(pages);
-        if (pages > availableInk) {
-            cout << "잉크가 부족합니다." << endl;
-            return;
-        }
-        availableInk -= pages;
-    }
+	Ink(string model, string manuf, int count, int ink) : Printer(model, manuf, count) {//명시적으로 기본 클래스 Printer의 생성자를 호출
+		this->availableInk = ink;
+	}
+	bool printInkJek(int pages = 0) {
+		if (print(pages)) {
+			if (availableInk < pages) {
+				cout << "잉크가 부족하여 프린트 할 수 없습니다" << endl;
+				return false;
+			}
+			else
+			{
+				this->availableInk -= pages;
+				return true;
+			}
+		}
+		else{
+			return false;
+		}
+	}
+	void show() {
+		cout << getModel() << " : " << getManu() << " ,남은 종이" << getCount() << "장 ,남은 잉크" << availableInk << endl;
+	}
 };
 
-class printerLaser : public printer {
-    int availableToner;
+class Laser :virtual public Printer {
+	int availableToner;//남은 토너양
 public:
-    printerLaser(int pages, int availableToner) : printer(pages), availableToner(availableToner) {}
-    int getToner() { return availableToner; }
-    void printLaser(int pages) {
-        print(pages);
-        if (pages > availableToner * 2) {
-            cout << "토너가 부족합니다." << endl;
-            return;
-        }
-        availableToner -= (pages / 2);
-    }
+	Laser(string model, string manuf, int count, int toner) : Printer(model, manuf, count) {
+		this->availableToner = toner;
+	}
+	bool printInkLaser(int pages = 0) {
+		if (print(pages)) {
+			if (availableToner < pages) {
+				cout << "잉크가 부족하여 프린트 할 수 없습니다" << endl;
+				return false;
+			}
+			else
+			{
+				this->availableToner -= pages;
+				return true;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	void show() {
+		cout << getModel() << " ," << getManu() << " ,남은 종이" << getCount() << "장 ,남은 토너" << availableToner << endl;
+	}
+
 };
+
 
 int main() {
-    string InkModel = "Officejet V40";
-    string LaserModel = "SCX-6x45";
-    string InkManufacturer = "HP";
-    string LaserManufacturer = "삼성전자";
+	Ink* ink = new Ink("Officejet v40","HP", 5, 10);
+	Laser* laser = new Laser("SCX-6x45", "삼성전자", 3, 20);
 
-    printerInkJet pj(5, 10);
-    printerLaser pl(10, 20);
-    pj.setModel(InkModel);
-    pj.setManufacturer(InkManufacturer);
-    pl.setModel(LaserModel);
-    pl.setManufacturer(LaserManufacturer);
+	cout << "현재 작동중인 2 대의 프린터는 아래와 같다" << endl;
+	cout << "잉크젯 : ";
+	ink->show();
+	cout << "레이저 : ";
+	laser->show();
 
-    cout << "현재 작동 중인 프린터는 아래와 같다." << endl;
-    cout << "잉크젯 : " << pj.getModel() << ", " << pj.getManufacturer() << ", "
-        << "남은 종이 " << pj.getPages() << ", " << "남은 잉크 " << pj.getInk() << endl;
-    cout << "레이저 : " << pl.getModel() << ", " << pl.getManufacturer() << ", "
-        << "남은 종이 " << pl.getPages() << ", " << "남은 토너 " << pl.getToner() << endl;
+	char chek = 'y';
+	int type, num;
 
-    int printerType;
-    int page;
-    char answer;
+	while (true) {
+		if (chek == 'y') {
+			cout << endl;
+			cout << "프린터(1:잉크젯, 2:레이저)와 매수 입력>>";
+			cin >> type >> num;
+			if (type == 1) {
+				if (ink->printInkJek(num)) {
+					cout << "프린트하였습니다." << endl;
+				}
+				ink->show();
+				laser->show();
+				cout << "계속 프린트 하시겠습니까(y/n)>>";
+				cin >> chek;
+				continue;
+			}
+			else if(type == 2){
+				if (laser->printInkLaser(num)) {
+					cout << "프린트하였습니다." << endl;
+				}
+				ink->show();
+				laser->show();
+				cout << "계속 프린트 하시겠습니까(y/n)>>";
+				cin >> chek;
+				continue;
+			}
+		}
+		else if (chek == 'n') {
+			break;
+		}
 
-    while (true) {
-        cout << "프린터(1:잉크젯, 2:레이저)와 매수 입력>> ";
-        cin >> printerType >> page;
-
-        if (printerType == 1) {
-            pj.printInkJet(page);
-            cout << "잉크젯 : " << pj.getModel() << ", " << pj.getManufacturer() << ", "
-                << "남은 종이 " << pj.getPages() << ", " << "남은 잉크 " << pj.getInk() << endl;
-        }
-        else if (printerType == 2) {
-            pl.printLaser(page);
-            cout << "레이저 : " << pl.getModel() << ", " << pl.getManufacturer() << ", "
-                << "남은 종이 " << pl.getPages() << ", " << "남은 토너 " << pl.getToner() << endl;
-        }
-
-        do {
-            cout << "계속 프린트 하시겠습니까?(y/n)>>";
-            cin >> answer;
-            if (answer == 'y') break;
-            else if (answer == 'n') return 0;
-            else cout << "y/n 만 입력해주세요." << endl;
-        } while (true);
-    }
-
-    return 0;
+	}
+	delete ink;
+	delete laser;
 }
